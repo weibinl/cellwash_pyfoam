bb565f44934e3728648f3b965a03d66afb0ce1c1
weibinl <39772822+weibinl@users.noreply.github.com>
2018-08-14 16:48:21 -0400

    Add files via upload
    generate geometry via the FreeCAD. 
    Setup the environment, and draw a cylinder, save the file,

---
+++ freecad.py
@@ -0,0 +1,23 @@
+import sys 
+sys.path.append("/usr/lib/freecad/lib")
+FREECADPATH='/usr/lib/freecad/lib'
+#def import_fcstd(path_freecad):
+    #function to import the FreeCAD module
+sys.path.append('/usr/lib/freecad/lib')
+import FreeCAD
+#    try:
+ #       import FreeCAD as FreeCAD
+ #   except:
+  #      print "Could not import FreeCAD"
+  #      print "Are you using the right path"
+
+#import_fcstd(FREECADPATH)
+freecad_doc_name='python_scrit_test'
+freecad_doc_path='/home/weibin/vs/'
+freecad_extension='.fcstd' 
+# extension to use when sae freecad file
+
+working_doc=FreeCAD.newDocument(freecad_doc_name)
+cylinder_1=working_doc.addObject('Part::Cylinder','cylinder_1')
+cylinder_1.Radius=4
+working_doc.saveAs(freecad_doc_path+freecad_doc_name+freecad_extension)
